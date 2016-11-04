ec612ea299b239679a9f6e01ea2abc29ad472f40
Oliver Mensah <olivermensah96@gmail.com>
2016-11-04 07:36:51 +0000

    data added

--- task001.php
+++ task001.php
@@ -83,7 +83,7 @@ foreach ($arrayData as $data) {
 }
 echo '<br>';
 
-/*
+/*task007
 *Loop through cities
 */
 $largest_cities = array('Tokyo', 'Mexico City', 'New York City', 'Mumbai', 'Seoul', 'Shanghai', 'Lagos', 'Buenos Aires', 'Cairo', 'London');
@@ -94,15 +94,93 @@ foreach($largest_cities as $cities){
 echo '<br>';
 
 sort($largest_cities);
+ echo'<ul>';
 foreach($largest_cities as $cities){
   echo '<li>'.$cities.'</li><br>';
 }
+echo'</ul>';
 echo '<br>';
 array_push($largest_cities, 'Los Angeles', 'Calcutta',  'Osaka', 'Beijing');
-
 sort($largest_cities);
+echo'<ul>';
 foreach($largest_cities as $cities){
   echo '<li>'.$cities.'</li><br>';
 }
-
+echo'</ul>';
 echo '<br>';
+
+
+/*task008
+*Transportation
+*/
+echo 'Travel takes many forms, whether across town, across the country,
+ or around the world.';
+echo' Here is a list of some common modes of transportation: ';
+   if (empty($_POST['data'])) {
+     $mode_of_transportation=array('Automobile','Jet','Ferry','Subway');
+     $label="Add mode of transportation to array. Please separate entries with commas<br>";
+   }
+   else {
+     //get the user inputs plus the original data
+     $strinToArray1 = explode(',',$_POST['TransModeArray']);
+     $strinToArray2 = explode(',',$_POST['data']);
+     $mode_of_transportation = array_merge($strinToArray1,$strinToArray2);
+     $label=" Add more<br>";
+   }
+   foreach ($mode_of_transportation as $transportMode){
+     echo "<li>$transportMode</li>";
+   }
+   $strinToArray1=implode(',',$mode_of_transportation);
+ ?>
+ <form method="post">
+   <?php echo"$label"; ?>
+   <input type="text" name="data" maxlength="500"/>
+   <input type="hidden" name="TransModeArray" value="<?php echo"$strinToArray1"; ?>" /> <br/>
+   <input type="submit" value="Go!">
+ </form>
+
+<!--
+Task 0009 using associative array
+Using value to search for key with array_search method
+ -->
+ <?php
+ $Country_Cities = array('Japan'=>'Tokyo', 'Mexico'=>'Mexico City','USA'=>'New York City','India'=>'Mumbai',
+ 'Korea'=>'Seoul','China'=>'Shanghai', 'Nigeria'=>'Lagos','Argentina'=>'Buenos Aires','Egypt'=>'Cairo','England'=>'London');
+
+if(empty($_POST['data'])){
+  $label = "Select a City";
+  $status = " ";
+}
+if(isset($_POST['data'])&& isset($_POST['submit'])){
+  $label = "You made a choice";
+  $value = $_POST['data'];
+  $status = $value . ' is in '. array_search($value,  $Country_Cities);
+}
+?>
+<form  method="post">
+  <label><?php echo $label; ?></label>
+  <select name="data">
+    <?php foreach($Country_Cities as $key => $value){
+     echo "<option value='$value'>$value</option>";
+   }
+   ?>
+  </select>
+  <input type="submit" name="submit" value="Submit">
+</form>
+<p>
+  <?php echo $status; ?>
+</p>
+<?php
+echo "<br>";
+/*
+*task 10
+array into multi
+*/
+
+// $multiCity = array();
+// $len = count($largest_cities);
+// for($x=0; $x<$len/2; $x++){
+//   $multiCity[ $largest_cities[$x+1] ,$largest_cities[$x+] ];
+// }
+// print_r($multiCity);
+//  ?>
