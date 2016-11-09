adc0645fdaf85960a19bd991fd93a4b457db7e11
Oliver Mensah <olivermensah96@gmail.com>
2016-11-09 03:52:06 +0000

    now done with my first assignment

--- task014.php
+++ task014.php
@@ -23,29 +23,166 @@ $os=array(
   'Others'
 );
 class Select{
-  private $name;
-  private $value;
+  //Property
+  private $name;   //String variable.
+  private $value;  //Array variable.
 
-  public function __construct($name, $value)
-  {
-    $this->name = $name;
-    $this->value =$value;
+  //settor method for string name
+  public function setName($name){
+     $this->name = $name;
   }
-  public function setName($name)
-  {
-    $this->name  = $name;
+  //getter mehod
+  public function getName(){
+     return $this->name;
   }
-  public function getName()
-  {
-    return $this->name;
+  //settor method for an array of values
+  public function setValue($value){
+     if (!is_array($value)){
+        die("Error: value is not an array.");
+     }
+     $this->value = $value;
+   }
+   //get values
+  public function getValue(){
+     return $this->value;
   }
-  public function setValue($value)
-  {
-    if(is_array($value)){
-      $this->value = $value;
-    }else{
-      echo "<em>Not available options</em>";
-    }
+
+  //This method creates the actual select options. It is private,
+  //since there is no need for it outside the operations of the class.
+  private function makeOptions($value){
+     foreach($value as $v){
+        echo "<option value=\"$v\">" .ucwords($v). "</option>\n";
+      }
   }
-}
- ?>
+
+  //This method puts it all together to create the select field.
+  public function makeSelect(){
+     echo "<select name=\"" .$this->getName(). "\">\n";
+     //Create options.
+     $this->makeOptions($this->getValue());
+     echo "</select>" ;
+  }
+}//end class
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Classes , methods and array</title>
+</head>
+<body>
+<h2>User Registration<br /></h2>
+
+<?php
+  if(!isset($_POST['submit'])){
+?>
+<form method="post" action="">
+<p>Name:<br />
+<input type="text" name="name" size="60" />  </p>
+<p>Username:<br />
+<input type="text" name="username" size="60" /></p>
+<p>Email:<br />
+<input type="text" name="email" size="60" /></p>
+<p>Work Access:<br></p>
+<p> Primary Browser :
+<?php
+//Instantiate object.
+$select = new Select();
+//Set properties.
+$select->setName('browser');
+$select->setValue($browsers);
+$select->makeSelect();
+?>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+Speed:
+<?php
+//set propeties
+$select ->setName('speed');
+$select->setValue($speed);
+$select->makeSelect();
+
+?>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+os:
+<?php
+//set propeties
+$select ->setName('os');
+$select->setValue($os);
+$select->makeSelect();
+
+?>
+</p>
+
+<p>Home Access:<br></p>
+<p> Primary Browser :
+<?php
+//Instantiate object.
+$select = new Select();
+//Set properties.
+$select->setName('browser_home');
+$select->setValue($browsers);
+$select->makeSelect();
+?>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+Speed:
+<?php
+//set propeties
+$select ->setName('speed_home');
+$select->setValue($speed);
+$select->makeSelect();
+
+?>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+os:
+<?php
+//set propeties
+$select ->setName('os_home');
+$select->setValue($os);
+$select->makeSelect();
+unset($select);
+?>
+</p>
+
+<input type="submit" name="submit" value="Go" />
+
+</form>
+
+<?php
+  //If form submitted, process input.
+  }else{
+    //Could include code to send data to database here.
+    //Retrieve user responses.
+    $name=$_POST['name'];
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+    //The following variable has an altered name to avoid confusion.
+    $Browser=$_POST['browser'];
+    $OS  = $_POST['os'];
+    $Speed = $_POST['speed'];
+
+    $Browser_home=$_POST['browser_home'];
+    $OS_home  = $_POST['os_home'];
+    $Speed_home = $_POST['speed_home'];
+
+
+    //Confirm responses to user.
+    echo "Data collected about  $name: <br />";
+    echo "Username: $username<br />";
+    echo "Email: $email<br /> <br>";
+    echo "Work internet details<br>";
+    echo "Browser: $Browser<br />";
+    echo "OS: $OS <br>";
+    echo "Speed: $Speed<br><br>";
+    echo "Home internet details<br>";
+    echo "Browser: $Browser_home<br />";
+    echo "OS: $OS_home<br>";
+    echo "Speed: $Speed_home<br>";
+  }
+?>
+
+</body>
+</html>
